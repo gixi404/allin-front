566a92c3368437f9bf5aec8cdec03d175c1c6332
gixi <gixi.tsx@gmail.com>
2024-09-26 16:39:36 -0300

    botón para consultar producto

--- src/components/NoMatches.jsx
+++ src/components/NoMatches.jsx
@@ -1,8 +1,16 @@
 function NoMatches() {
   return (
-    <p className="text-lg lg:text-xl text-black tracking-tight">
-      No hay coincidencias...
-    </p>
+    <div className="flex flex-col items-center justify-center w-full gap-y-8">
+      <p className="text-lg lg:text-2xl text-black tracking-tight w-full text-center max-w-[450px] text-pretty">
+        El producto no está en el catálogo, pero puede que lo tengamos...
+      </p>
+      <a
+        href=""
+        className="bg-green-300 font-semibold px-6 sm:px-8 py-2 rounded-lg text-xl border-2 border-green-400 hover:bg-green-300 duration-100"
+      >
+        Consultar
+      </a>
+    </div>
   );
 }
 
