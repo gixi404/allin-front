186deafa6002a35ce4fdc8acc683da614b94c903
gixi <gixi.tsx@gmail.com>
2024-11-04 21:21:35 -0300

    cyber monday

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import CreateYourApp from "../components/CreateYourApp";
 import Main from "../components/Main";
 import SecurityCameras from "../components/SecurityCameras";
+import cyberMonday from "/cybermonday.png";
 
 function Home() {
   return (
     <>
+      <img
+        src={cyberMonday}
+        alt="Cyber Monday"
+        className="sm:w-[70%] sm:-mt-10 rounded-lg sm:h-[400px] h-[500px] px-4 sm:px-0"
+      />
       <Main />
       <CreateYourApp />
       <SecurityCameras />
